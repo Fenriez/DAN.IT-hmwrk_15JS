040a48bf9763533abd94e2a377aee9ce06db4f55
Fenriez <andy.dolot@gmail.com>
2019-01-21 14:10:52 +0200

    All features are working properly

--- script/main.js
+++ script/main.js
@@ -2,39 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
     let timer_block = document.getElementById('timer'),
         trigger_button = document.getElementById('trigger'),
         reset_button = document.getElementById('reset'),
-        timer = false,
+        timer = 0,
         startTime = 0,
         currentTime = 0,
-        elapsedTime = 0;
+        showTime = 0,
+        elapsedTime = 0,
+        currentInterval = 0;
 
     trigger_button.addEventListener('click', triggerTimer);
     reset_button.addEventListener('click', reset);
 
     function triggerTimer() {
-        console.log(timer);
         if (!timer) {
-            if (startTime == 0) {
-                startTime = new Date();
-            } else {
-                startTime = new Date(startTime);
-            }
+            startTime = new Date();
             timer = setInterval(() => {
                 currentTime = new Date();
-                elapsedTime = new Date(currentTime - startTime);
-                timer_block.innerText = `${elapsedTime.getMinutes()}:${elapsedTime.getSeconds()}:${elapsedTime.getMilliseconds()}`;
+                currentInterval = (currentTime - startTime);
+                showTime = currentInterval + elapsedTime;
+                showTime = new Date(showTime);
+                timer_block.innerText = `${showTime.getMinutes()}:${showTime.getSeconds()}:${showTime.getMilliseconds()}`;
             }, 1);
             trigger_button.innerText = 'STOP';
         } else {
-            startTime = +startTime + +elapsedTime;
-            timer = clearInterval(timer);
+            clearInterval(timer);
+            timer = 0;
+            elapsedTime += currentInterval;
             trigger_button.innerText = 'START';
         }
     };
 
     function reset() {
         if (startTime != 0) {
-            timer = clearInterval(timer);
-            startTime = 0;
+            clearInterval(timer);
+            timer = 0;            
+            startTime = elapsedTime = 0;
             timer_block.innerText = '00:00:000';
         }
     }
